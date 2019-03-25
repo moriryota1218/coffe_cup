3450ce6b2234df78a4a4b8475d3a9e24f4734af7
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2019-03-25 21:35:52 +0900

    update3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts, :only => [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :welcome, :only => [:index]
   root "welcome#index"
