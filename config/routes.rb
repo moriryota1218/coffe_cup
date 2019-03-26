85acdd4b4f9ecfd0c6b9d82ab07289065cb5112e
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2019-03-26 21:50:52 +0900

    update4

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :posts, :only => [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :welcome, :only => [:index]
   root "welcome#index"
+  resources :users, :only => [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
